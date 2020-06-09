78d26a6e30083afa8b42ab4d90d87dbdcb07192d
courtneycooksey <courtney.cooksey@thecrossingchurch.com>
2020-06-09 10:38:19 -0600

    Split Group Sceduler Job into an initail email and follow-up text

--- org.thecrossingchurch.CustomJobs/Jobs/GroupSchedulerFollowUp.cs
+++ org.thecrossingchurch.CustomJobs/Jobs/GroupSchedulerFollowUp.cs
@@ -29,24 +29,22 @@ using System.Web.UI;
 using System.Linq.Expressions;
 using DotLiquid.Util;
 
-namespace org.crossingchurch.SimpleGroupScheduler.Jobs
+namespace org.crossingchurch.GroupSchedulerFollowUp.Jobs
 {
     /// <summary>
     /// 
     /// </summary>
     [GroupField("Group", "Group to Auto-Schedule", true, "", "", 0)]
     [BooleanField("Include Child Groups", "If checked, the job will schedule all child groups of the selected parent", false, "", 0)]
-    [PersonField("Sender", "The person the message about serving should come from", true)]
     [TextField("RSVP Url", "The URL of the Serving RSVP Page", true)]
     [DisallowConcurrentExecution]
-    public class SimpleGroupScheduler : IJob
+    public class GroupSchedulerFollowUp : IJob
     {
         private GroupService GroupSvc { get; set; }
         private AttendanceOccurrenceService AttendanceOccurrenceSvc { get; set; }
         private AttendanceService AttendanceSvc { get; set; }
         private CommunicationService CommunicationSvc { get; set; }
         private PersonService PersonSvc { get; set; }
-        private Person Sender { get; set; }
         private RockContext _context { get; set; }
         private string baseurl { get; set; }
 
@@ -57,7 +55,7 @@ namespace org.crossingchurch.SimpleGroupScheduler.Jobs
         /// scheduler can instantiate the class whenever it needs.
         /// </para>
         /// </summary>
-        public SimpleGroupScheduler() { }
+        public GroupSchedulerFollowUp() { }
 
         /// <summary>
         /// Job that will create attendance for all group members and then email them for RSVP.
@@ -71,7 +69,7 @@ namespace org.crossingchurch.SimpleGroupScheduler.Jobs
             JobDataMap dataMap = context.JobDetail.JobDataMap;
             _context = new RockContext();
             List<Group> groups = new List<Group>();
-            List<Person> scheduledPeople = new List<Person>();
+            List<Person> noResponsePeople = new List<Person>();
             GroupSvc = new GroupService(_context);
             AttendanceOccurrenceSvc = new AttendanceOccurrenceService(_context);
             AttendanceSvc = new AttendanceService(_context);
@@ -82,7 +80,6 @@ namespace org.crossingchurch.SimpleGroupScheduler.Jobs
             bool includeChildren = dataMap.GetBoolean("IncludeChildGroups");
             string sender_guid = dataMap.GetString("Sender");
             baseurl = dataMap.GetString("RSVPUrl");
-            Sender = new PersonAliasService(_context).Get(Guid.Parse(sender_guid)).Person;
 
             Group parent = GroupSvc.Get(Guid.Parse(guid));
             if ( includeChildren )
@@ -99,17 +96,17 @@ namespace org.crossingchurch.SimpleGroupScheduler.Jobs
                 var members = groups[i].Members.ToList();
                 for ( var j = 0; j < members.Count(); j++ )
                 {
-                    ScheduleMember(groups[i], members[j].Person);
-                    if ( !scheduledPeople.Select(p => p.Id).Contains(members[j].Person.Id) )
+                    var hasResponded = HasResponded(groups[i], members[j].Person);
+                    if ( !hasResponded && !noResponsePeople.Select(p => p.Id).Contains(members[j].Person.Id) )
                     {
-                        scheduledPeople.Add(members[j].Person);
+                        noResponsePeople.Add(members[j].Person);
                     }
                 }
             }
             //Send Notifications to Scheduled People
-            for ( var i = 0; i < scheduledPeople.Count(); i++ )
+            for ( var i = 0; i < noResponsePeople.Count(); i++ )
             {
-                SendMessage(scheduledPeople[i]);
+                SendMessage(noResponsePeople[i]);
             }
         }
 
@@ -128,8 +125,9 @@ namespace org.crossingchurch.SimpleGroupScheduler.Jobs
             return list;
         }
 
-        private void ScheduleMember( Group group, Person person )
+        private bool HasResponded( Group group, Person person )
         {
+            //Check to see if Person has responded to invitations
             var locations = group.GroupLocations.ToList();
             for ( var i = 0; i < locations.Count(); i++ )
             {
@@ -147,11 +145,13 @@ namespace org.crossingchurch.SimpleGroupScheduler.Jobs
                         //Add new occurrence if none exist
                         nextOcc = GetNextOccurrence(schedules[j]);
                     }
-                    var occurrence = AttendanceOccurrenceSvc.GetOrAdd(nextOcc.Value, group.Id, locations[i].LocationId, schedules[j].Id);
-                    var attendance = AttendanceSvc.ScheduledPersonAddPending(person.Id, occurrence.Id, Sender.PrimaryAlias);
-                    _context.SaveChanges();
+                    var attendance = AttendanceSvc.Get(nextOcc.Value, locations[i].LocationId, schedules[j].Id, group.Id, person.Id);
+                    if ( attendance.RSVP == RSVP.Yes || attendance.RSVP == RSVP.No ) {
+                        return true;
+                    }
                 }
             }
+            return false;
         }
 
         private void SendMessage( Person person )
@@ -159,50 +159,29 @@ namespace org.crossingchurch.SimpleGroupScheduler.Jobs
             var token = person.GetImpersonationToken();
             var url = baseurl + "?rckipid=" + token;
             var msg = "Your serving team is scheduled this week, please use this <a href='" + url + "'>link</a> to let us know if you are able to serve.";
-            //Send Email
-            var header = new AttributeValueService(_context).Queryable().FirstOrDefault(a => a.AttributeId == 140).Value; //Email Header
-            var footer = new AttributeValueService(_context).Queryable().FirstOrDefault(a => a.AttributeId == 141).Value; //Email Footer 
-            var message = header + msg + footer;
-            string subject = "Your Serving Team is Scheduled This Week";
-            RockEmailMessageRecipient recipient = new RockEmailMessageRecipient(person, new Dictionary<string, object>());
-            RockEmailMessage email = new RockEmailMessage();
-            email.Subject = subject;
-            email.Message = message;
-            email.FromEmail = "info@thecrossingchurch.com";
-            email.FromName = "The Crossing System";
-            email.AddRecipient(recipient);
-            var output = email.Send();
-            #region sms 
-            //if ( person.CommunicationPreference == CommunicationType.SMS )
-            //{
-            //    //Get 65201 Phone
-            //    DefinedValue phoneDV = new DefinedValueService(_context).Get(1702);
-            //    var cache = new DefinedValueCache();
-            //    cache.SetFromEntity(phoneDV);
-            //    //Send SMS
-            //    PageShortLinkService ShortLinkSvc = new PageShortLinkService(_context);
-            //    //Create Short Link
-            //    var tkn = ShortLinkSvc.GetUniqueToken(3, 7);
-            //    var link = new PageShortLink();
-            //    link.SiteId = 3; //The Crossing External Site
-            //    link.Token = tkn;
-            //    link.Url = url;
-            //    ShortLinkSvc.Add(link);
-            //    _context.SaveChanges();
-            //    link = ShortLinkSvc.GetInclude(link.Id, l => l.Site);
-            //    //Set Message
-            //    msg = "Your serving team is scheduled this week, please use this link to let us know if you are able to serve. " + link.Site.SiteDomains.First() + "/" + link.Token;
-            //    RockSMSMessageRecipient recipient = new RockSMSMessageRecipient(person, person.PhoneNumbers.FirstOrDefault(p => p.NumberTypeValue.Value == "Mobile").Number, new Dictionary<string, object>());
-            //    RockSMSMessage sms = new RockSMSMessage();
-            //    sms.Message = msg;
-            //    sms.FromNumber = cache;
-            //    sms.AddRecipient(recipient);
-            //    var output = sms.Send();
-            //}
-            //else
-            //{
-            //}
-            #endregion
+            //Get 65201 Phone
+            DefinedValue phoneDV = new DefinedValueService(_context).Get(1702);
+            var cache = new DefinedValueCache();
+            cache.SetFromEntity(phoneDV);
+            //Send SMS
+            PageShortLinkService ShortLinkSvc = new PageShortLinkService(_context);
+            //Create Short Link
+            var tkn = ShortLinkSvc.GetUniqueToken(3, 7);
+            var link = new PageShortLink();
+            link.SiteId = 3; //The Crossing External Site
+            link.Token = tkn;
+            link.Url = url;
+            ShortLinkSvc.Add(link);
+            _context.SaveChanges();
+            link = ShortLinkSvc.GetInclude(link.Id, l => l.Site);
+            //Set Message
+            msg = "Your serving team is scheduled this week, please use this link to let us know if you are able to serve. " + link.Site.SiteDomains.First() + "/" + link.Token;
+            RockSMSMessageRecipient recipient = new RockSMSMessageRecipient(person, person.PhoneNumbers.FirstOrDefault(p => p.NumberTypeValue.Value == "Mobile").Number, new Dictionary<string, object>());
+            RockSMSMessage sms = new RockSMSMessage();
+            sms.Message = msg;
+            sms.FromNumber = cache;
+            sms.AddRecipient(recipient);
+            var output = sms.Send();
         }
 
         private DateTime GetNextOccurrence( Schedule schedule )
