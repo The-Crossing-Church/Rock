974634b283aebb89b6576d7eedd7561b5e951be6
courtneycooksey <courtney.cooksey@thecrossingchurch.com>
2020-12-16 13:50:14 -0700

    Add check for anonymous donor

--- org.thecrossingchurch.CustomJobs/Jobs/VeritasSupportChanges.cs
+++ org.thecrossingchurch.CustomJobs/Jobs/VeritasSupportChanges.cs
@@ -34,7 +34,7 @@ namespace org.crossingchurch.VeritasSupportChanges.Jobs
     /// <summary>
     /// 
     /// </summary>
-    [GroupField("Fundraising Group", "Veritas Fundraising Group", true, "", "", 0)]
+    [GroupField( "Fundraising Group", "Veritas Fundraising Group", true, "", "", 0 )]
     [DisallowConcurrentExecution]
     public class VeritasSupportChanges : IJob
     {
@@ -61,13 +61,13 @@ namespace org.crossingchurch.VeritasSupportChanges.Jobs
             JobDataMap dataMap = context.JobDetail.JobDataMap;
             _context = new RockContext();
 
-            string guid = dataMap.GetString("FundraisingGroup");
-            int groupId = new GroupService(_context).Get(Guid.Parse(guid)).Id;
-            var members = new GroupMemberService(_context).Queryable().Where(gm => gm.GroupId == groupId).ToList();
+            string guid = dataMap.GetString( "FundraisingGroup" );
+            int groupId = new GroupService( _context ).Get( Guid.Parse( guid ) ).Id;
+            var members = new GroupMemberService( _context ).Queryable().Where( gm => gm.GroupId == groupId ).ToList();
             for ( var i = 0; i < members.Count(); i++ )
             {
                 //Get transaction data
-                LoadTransactionData(members[i]);
+                LoadTransactionData( members[i] );
             }
         }
 
@@ -78,10 +78,10 @@ namespace org.crossingchurch.VeritasSupportChanges.Jobs
         {
             //Load Attibutes and find Primary Fundraising Account
             groupMember.LoadAttributes();
-            string accountGuid = groupMember.GetAttributeValue("FundraisingAccount");
-            FinancialAccount account = new FinancialAccountService(_context).Get(Guid.Parse(accountGuid));
-            List<FinancialTransaction> transactions = new FinancialTransactionService(_context).Queryable().Where(ft => ft.TransactionDetails.Any(td => td.AccountId == account.Id)).ToList();
-            ProcessTransactions(transactions, groupMember, account);
+            string accountGuid = groupMember.GetAttributeValue( "FundraisingAccount" );
+            FinancialAccount account = new FinancialAccountService( _context ).Get( Guid.Parse( accountGuid ) );
+            List<FinancialTransaction> transactions = new FinancialTransactionService( _context ).Queryable().Where( ft => ft.TransactionDetails.Any( td => td.AccountId == account.Id ) ).ToList();
+            ProcessTransactions( transactions, groupMember, account );
         }
 
         /// <summary>
@@ -89,9 +89,9 @@ namespace org.crossingchurch.VeritasSupportChanges.Jobs
         /// </summary>
         private void ProcessTransactions( List<FinancialTransaction> transactions, GroupMember groupMember, FinancialAccount account )
         {
-            DateTime dt = DateTime.Now.AddDays(-1);
-            dt = new DateTime(dt.Year, dt.Month, dt.Day, 0, 0, 0);
-            var current = transactions.Where(t => DateTime.Compare(dt, t.TransactionDateTime.Value) <= 0).ToList();
+            DateTime dt = DateTime.Now.AddDays( -1 );
+            dt = new DateTime( dt.Year, dt.Month, dt.Day, 0, 0, 0 );
+            var current = transactions.Where( t => DateTime.Compare( dt, t.TransactionDateTime.Value ) <= 0 ).ToList();
             var message = "";
             for ( var i = 0; i < current.Count(); i++ )
             {
@@ -100,7 +100,7 @@ namespace org.crossingchurch.VeritasSupportChanges.Jobs
 
                 //Check if this is first contribution to this account
                 int contributor = current[i].AuthorizedPersonAliasId.Value;
-                if ( !transactions.Any(t => t.AuthorizedPersonAliasId == contributor && t.Id != current[i].Id) )
+                if ( !transactions.Any( t => t.AuthorizedPersonAliasId == contributor && t.Id != current[i].Id ) )
                 {
                     isFirst = true;
                 }
@@ -108,8 +108,8 @@ namespace org.crossingchurch.VeritasSupportChanges.Jobs
                 //Check if the scheduled transaction has changed
                 if ( current[i].ScheduledTransactionId.HasValue )
                 {
-                    FinancialTransaction lastTransaction = transactions.Where(t => t.ScheduledTransactionId == current[i].ScheduledTransactionId && t.Id != current[i].Id).OrderByDescending(t => t.TransactionDateTime).FirstOrDefault();
-                    if ( lastTransaction != null && lastTransaction.TransactionDetails.FirstOrDefault(td => td.AccountId == account.Id).Amount != current[i].TransactionDetails.FirstOrDefault(td => td.AccountId == account.Id).Amount )
+                    FinancialTransaction lastTransaction = transactions.Where( t => t.ScheduledTransactionId == current[i].ScheduledTransactionId && t.Id != current[i].Id ).OrderByDescending( t => t.TransactionDateTime ).FirstOrDefault();
+                    if ( lastTransaction != null && lastTransaction.TransactionDetails.FirstOrDefault( td => td.AccountId == account.Id ).Amount != current[i].TransactionDetails.FirstOrDefault( td => td.AccountId == account.Id ).Amount )
                     {
                         isChanged = true;
                     }
@@ -118,74 +118,74 @@ namespace org.crossingchurch.VeritasSupportChanges.Jobs
                 //Add this transaction to message if different
                 if ( isChanged || isFirst )
                 {
-                    Person person = new PersonAliasService(_context).Get(contributor).Person;
+                    Person person = new PersonAliasService( _context ).Get( contributor ).Person;
                     //Title for Section
                     message += "<p style='font-size: 18px; font-weight: bold;'>";
                     if ( isFirst )
                     {
-                        message += "New Contibution From " + person.FullName;
+                        message += "New Contibution From " + ( current[i].ShowAsAnonymous == true ? "an Anonymous Donor" : person.FullName );
                     }
                     else
                     {
-                        message += "Recurring Contribution From " + person.FullName + " Has Changed";
+                        message += "Recurring Contribution From " + ( current[i].ShowAsAnonymous == true ? "an Anonymous Donor" : person.FullName ) + " Has Changed";
                     }
                     message += "</p>\n<br/>";
                     //Information about transaction
                     message += "<p style='font-size: 14px;'>";
-                    message += "Amount: $" + current[i].TransactionDetails.FirstOrDefault(td => td.AccountId == account.Id).Amount + "\n<br/>";
+                    message += "Amount: $" + current[i].TransactionDetails.FirstOrDefault( td => td.AccountId == account.Id ).Amount + "\n<br/>";
                     if ( isChanged )
                     {
-                        FinancialTransaction lastTransaction = transactions.Where(t => t.ScheduledTransactionId == current[i].ScheduledTransactionId && t.Id != current[i].Id).OrderByDescending(t => t.TransactionDateTime).First();
-                        message += "Previous Amount: $" + lastTransaction.TransactionDetails.FirstOrDefault(td => td.AccountId == account.Id).Amount + "\n<br/>";
+                        FinancialTransaction lastTransaction = transactions.Where( t => t.ScheduledTransactionId == current[i].ScheduledTransactionId && t.Id != current[i].Id ).OrderByDescending( t => t.TransactionDateTime ).First();
+                        message += "Previous Amount: $" + lastTransaction.TransactionDetails.FirstOrDefault( td => td.AccountId == account.Id ).Amount + "\n<br/>";
                     }
-                    message += GetMobileNumber(person);
-                    message += GetAddress(person);
+                    message += ( current[i].ShowAsAnonymous == true ? "" : GetMobileNumber( person ) );
+                    message += ( current[i].ShowAsAnonymous == true ? "" : GetAddress( person ) );
                     message += "</p>\n<br/>\n<br/>";
                 }
             }
 
             //Check for potentially failed payments
             DateTime today = DateTime.Now;
-            today = new DateTime(today.Year, today.Month, today.Day, 0, 0, 0);
-            var scheduled = new FinancialScheduledTransactionService(_context).Queryable().Where(fst => fst.IsActive && fst.ScheduledTransactionDetails.Any(fstd => fstd.AccountId == account.Id) && DateTime.Compare(dt, fst.NextPaymentDate.Value) <= 0 && DateTime.Compare(today, fst.NextPaymentDate.Value) > 0).ToList();
-            var failed = scheduled.Where(s => !current.Select(c => c.ScheduledTransactionId).Contains(s.Id)).ToList();
+            today = new DateTime( today.Year, today.Month, today.Day, 0, 0, 0 );
+            var scheduled = new FinancialScheduledTransactionService( _context ).Queryable().Where( fst => fst.IsActive && fst.ScheduledTransactionDetails.Any( fstd => fstd.AccountId == account.Id ) && DateTime.Compare( dt, fst.NextPaymentDate.Value ) <= 0 && DateTime.Compare( today, fst.NextPaymentDate.Value ) > 0 ).ToList();
+            var failed = scheduled.Where( s => !current.Select( c => c.ScheduledTransactionId ).Contains( s.Id ) ).ToList();
             for ( var i = 0; i < failed.Count(); i++ )
             {
-                Person person = new PersonAliasService(_context).Get(failed[i].AuthorizedPersonAliasId).Person;
+                Person person = new PersonAliasService( _context ).Get( failed[i].AuthorizedPersonAliasId ).Person;
                 //Title for Section
                 message += "<p style='font-size: 18px; font-weight: bold;'>";
-                message += "Potentially Failed Payment from " + person.FullName;
+                message += "Potentially Failed Payment from " + ( current[i].ShowAsAnonymous == true ? "an Anonymous Donor" : person.FullName );
                 message += "</p>\n<br/>";
                 //Information about transaction
                 message += "<p style='font-size: 14px;'>";
-                message += "Amount: $" + failed[i].ScheduledTransactionDetails.FirstOrDefault(td => td.AccountId == account.Id).Amount + "\n<br/>";
-                message += GetMobileNumber(person);
-                message += GetAddress(person);
+                message += "Amount: $" + failed[i].ScheduledTransactionDetails.FirstOrDefault( td => td.AccountId == account.Id ).Amount + "\n<br/>";
+                message += ( current[i].ShowAsAnonymous == true ? "" : GetMobileNumber( person ) );
+                message += ( current[i].ShowAsAnonymous == true ? "" : GetAddress( person ) );
                 message += "</p>\n<br/>\n<br/>";
             }
 
             //Send Message
             if ( message.IsNotNullOrWhiteSpace() )
             {
-                var header = new AttributeValueService(_context).Queryable().FirstOrDefault(a => a.AttributeId == 140).Value; //Email Header
-                var footer = new AttributeValueService(_context).Queryable().FirstOrDefault(a => a.AttributeId == 141).Value; //Email Footer 
+                var header = new AttributeValueService( _context ).Queryable().FirstOrDefault( a => a.AttributeId == 140 ).Value; //Email Header
+                var footer = new AttributeValueService( _context ).Queryable().FirstOrDefault( a => a.AttributeId == 141 ).Value; //Email Footer 
                 message = header + "<p style='font-size: 20px; font-weight: bold;'>Contibution Changes for " + account.Name + "</p>\n<br/>" + message + footer;
                 string subject = "Contibution Changes for " + account.Name;
-                RockEmailMessageRecipient recipient = new RockEmailMessageRecipient(groupMember.Person, new Dictionary<string, object>());
+                RockEmailMessageRecipient recipient = new RockEmailMessageRecipient( groupMember.Person, new Dictionary<string, object>() );
                 RockEmailMessage email = new RockEmailMessage();
                 email.Subject = subject;
                 email.Message = message;
                 email.FromEmail = "info@thecrossingchurch.com";
                 email.FromName = "The Crossing System";
-                email.AddRecipient(recipient);
+                email.AddRecipient( recipient );
                 var output = email.Send();
-                Console.WriteLine(output);
+                Console.WriteLine( output );
             }
         }
 
         private string GetMobileNumber( Person person )
         {
-            var phone = person.PhoneNumbers.FirstOrDefault(p => p.NumberTypeValue.Value == "Mobile");
+            var phone = person.PhoneNumbers.FirstOrDefault( p => p.NumberTypeValue.Value == "Mobile" );
             if ( phone == null )
             {
                 phone = person.PhoneNumbers.FirstOrDefault();
