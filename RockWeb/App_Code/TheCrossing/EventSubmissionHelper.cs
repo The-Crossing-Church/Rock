9bf3c81625c760c4c5dbc216d460721353abb26a
courtneycooksey <courtney.cooksey@thecrossingchurch.com>
2021-12-08 09:29:17 -0700

    Add event submission helper for reusable code

--- RockWeb/App_Code/TheCrossing/EventSubmissionHelper.cs
+++ RockWeb/App_Code/TheCrossing/EventSubmissionHelper.cs
@@ -23,6 +23,7 @@ namespace RockWeb.TheCrossing
         public List<DefinedValue> BudgetLines { get; set; }
         public string BudgetLinesJSON { get; set; }
         public int ContentChannelId { get; set; }
+        public int ContentChannelTypeId { get; set; }
         public string BaseURL { get; set; }
         #endregion
 
@@ -57,6 +58,7 @@ namespace RockWeb.TheCrossing
             {
                 ContentChannel channel = new ContentChannelService( context ).Get( ContentChannelGuid.Value );
                 ContentChannelId = channel.Id;
+                ContentChannelTypeId = channel.ContentChannelTypeId;
             }
 
             Rock.Model.Attribute attr = new AttributeService( context ).Queryable().FirstOrDefault( a => a.Key == "InternalApplicationRoot" );
@@ -68,6 +70,137 @@ namespace RockWeb.TheCrossing
                     BaseURL += "/";
                 }
             }
+        }
+
+        public string GetRequestResources( EventRequest request )
+        {
+            List<string> rt = new List<string>();
+            if ( request.needsSpace )
+            {
+                rt.Add( "Room" );
+            }
+            if ( request.needsOnline )
+            {
+                rt.Add( "Online Event" );
+            }
+            if ( request.needsPub )
+            {
+                rt.Add( "Publicity" );
+            }
+            if ( request.needsCatering )
+            {
+                rt.Add( "Catering" );
+            }
+            if ( request.needsChildCare )
+            {
+                rt.Add( "Childcare" );
+            }
+            if ( request.needsReg )
+            {
+                rt.Add( "Registration" );
+            }
+            if ( request.needsAccom )
+            {
+                rt.Add( "Extra Resources" );
+            }
+            return String.Join( ", ", rt );
+        }
+        public class EventRequest
+        {
+            public bool needsSpace { get; set; }
+            public bool needsOnline { get; set; }
+            public bool needsPub { get; set; }
+            public bool needsReg { get; set; }
+            public bool needsCatering { get; set; }
+            public bool needsChildCare { get; set; }
+            public bool needsAccom { get; set; }
+            public bool IsSame { get; set; }
+            public string Name { get; set; }
+            public string Ministry { get; set; }
+            public string Contact { get; set; }
+            public List<string> EventDates { get; set; }
+            public List<EventDetails> Events { get; set; }
+            public string WhyAttendSixtyFive { get; set; }
+            public string TargetAudience { get; set; }
+            public bool EventIsSticky { get; set; }
+            public DateTime? PublicityStartDate { get; set; }
+            public DateTime? PublicityEndDate { get; set; }
+            public List<string> PublicityStrategies { get; set; }
+            public string WhyAttendNinety { get; set; }
+            public List<string> GoogleKeys { get; set; }
+            public string WhyAttendTen { get; set; }
+            public string VisualIdeas { get; set; }
+            public List<StoryItem> Stories { get; set; }
+            public string WhyAttendTwenty { get; set; }
+            public string Notes { get; set; }
+            public bool HasConflicts { get; set; }
+        }
+        public class StoryItem
+        {
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public string Description { get; set; }
+        }
+        public class EventDetails
+        {
+            public string EventDate { get; set; }
+            public string StartTime { get; set; }
+            public string EndTime { get; set; }
+            public int? MinsStartBuffer { get; set; }
+            public int? MinsEndBuffer { get; set; }
+            public int? ExpectedAttendance { get; set; }
+            public List<string> Rooms { get; set; }
+            public List<string> TableType { get; set; }
+            public int? NumTablesRound { get; set; }
+            public int? NumTablesRect { get; set; }
+            public int? NumChairsRound { get; set; }
+            public int? NumChairsRect { get; set; }
+            public bool? Checkin { get; set; }
+            public bool? SupportTeam { get; set; }
+            public string EventURL { get; set; }
+            public string ZoomPassword { get; set; }
+            public DateTime? RegistrationDate { get; set; }
+            public DateTime? RegistrationEndDate { get; set; }
+            public string RegistrationEndTime { get; set; }
+            public List<string> FeeType { get; set; }
+            public string FeeBudgetLine { get; set; }
+            public string Fee { get; set; }
+            public string CoupleFee { get; set; }
+            public string OnlineFee { get; set; }
+            public string Sender { get; set; }
+            public string SenderEmail { get; set; }
+            public string ThankYou { get; set; }
+            public string TimeLocation { get; set; }
+            public string AdditionalDetails { get; set; }
+            public string Vendor { get; set; }
+            public string Menu { get; set; }
+            public bool FoodDelivery { get; set; }
+            public string FoodTime { get; set; }
+            public string BudgetLine { get; set; }
+            public string FoodDropOff { get; set; }
+            public string CCVendor { get; set; }
+            public string CCMenu { get; set; }
+            public string CCFoodTime { get; set; }
+            public string CCBudgetLine { get; set; }
+            public List<string> ChildCareOptions { get; set; }
+            public int? EstimatedKids { get; set; }
+            public string CCStartTime { get; set; }
+            public string CCEndTime { get; set; }
+            public List<string> Drinks { get; set; }
+            public string DrinkDropOff { get; set; }
+            public string DrinkTime { get; set; }
+            public List<string> TechNeeds { get; set; }
+            public bool ShowOnCalendar { get; set; }
+            public string PublicityBlurb { get; set; }
+            public string TechDescription { get; set; }
+            public string SetUp { get; set; }
+            public bool? NeedsDoorsUnlocked { get; set; }
+        }
+        public class Comment
+        {
+            public string CreatedBy { get; set; }
+            public DateTime? CreatedOn { get; set; }
+            public string Message { get; set; }
         }
     }
 }
