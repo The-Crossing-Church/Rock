f2ad854ebab86c5fc4e971cdba9fa8c4df5e895b
courtneycooksey <courtney.cooksey@thecrossingchurch.com>
2023-10-05 12:24:16 -0500

    Added custom page title control to not render when empty

---
+++ org.crossingchurch.OurRock/Web/UI/Controls/PageTitle.cs
@@ -0,0 +1,62 @@
+﻿// <copyright>
+// Copyright by the Spark Development Network
+//
+// Licensed under the Rock Community License (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.rockrms.com/license
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using System.Web.UI;
+
+using Rock.Web.Cache;
+using Rock.Web.UI;
+
+namespace org.crossingchurch.OurRock.Web.UI.Controls
+{
+    /// <summary>
+    /// Renders the title of a page
+    /// </summary>
+    public class PageTitle : Control
+    {
+        public string Tag { get; set; }
+        public string CssClasses { get; set; }
+        /// <summary>
+        /// Outputs server control content to a provided <see cref="T:System.Web.UI.HtmlTextWriter" /> object and stores tracing information about the control if tracing is enabled.
+        /// </summary>
+        /// <param name="writer">The <see cref="T:System.Web.UI.HtmlTextWriter" /> object that receives the control content.</param>
+        public override void RenderControl( HtmlTextWriter writer )
+        {
+            var rockPage = this.Page as RockPage;
+            if (rockPage != null)
+            {
+                var pageCache = PageCache.Get( rockPage.PageId );
+                if (pageCache != null && pageCache.PageDisplayTitle && !string.IsNullOrWhiteSpace( rockPage.PageTitle ))
+                {
+                    if (!String.IsNullOrEmpty( Tag ))
+                    {
+                        string content = $"<{Tag} ";
+                        if (!String.IsNullOrEmpty( CssClasses ))
+                        {
+                            content += $"class='{CssClasses}'";
+                        }
+                        content += $">{rockPage.PageTitle}</{Tag}>";
+                        writer.Write( content );
+                    }
+                    else
+                    {
+                        writer.Write( rockPage.PageTitle );
+                    }
+                }
+            }
+        }
+    }
+}
