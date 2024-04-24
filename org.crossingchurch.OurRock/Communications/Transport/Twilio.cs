38e81ece7ebcd93f9653dcf33026849ce9f6292f
David Turner <david@pillarsforrock.com>
2024-04-24 11:47:49 -0700

    Update custom Twilio transport to override all new methods so that when SMS messages are sent through a job or workflow action, the Stop is still honored.

--- org.crossingchurch.OurRock/Communications/Transport/Twilio.cs
+++ org.crossingchurch.OurRock/Communications/Transport/Twilio.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.Composition;
+using System.Globalization;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 using Rock;
@@ -47,6 +50,43 @@ namespace org.crossingchurch.OurRock.Communications.Transport
             await base.SendAsync(communication, mediumEntityTypeId, mediumAttributes );
         }
 
+        public async new Task<SendMessageResult> SendAsync( RockMessage rockMessage, int mediumEntityTypeId, Dictionary<string, string> mediumAttributes )
+        {
+            var smsMessage = rockMessage as RockSMSMessage;
+            if ( smsMessage != null && smsMessage.FromSystemPhoneNumber != null ) 
+            {
+                var fromNumber = smsMessage.FromSystemPhoneNumber;
+                var stopGroup = fromNumber.GetAttributeValue( "StopGroup" ).AsGuidOrNull();
+                if ( stopGroup.HasValue )
+                {
+                    var stopPersonIds = new GroupMemberService( new RockContext() )
+                        .GetByGroupGuid( stopGroup.Value )
+                        .Select( m => m.PersonId )
+                    .ToList();
+
+                    smsMessage.SetRecipients( smsMessage.GetRecipients()
+                        .Where( r =>
+                            r.PersonId.HasValue &&
+                            !stopPersonIds.Contains( r.PersonId.Value ) )
+                        .ToList() );
+                }
+            }
+
+            return await base.SendAsync( rockMessage, mediumEntityTypeId, mediumAttributes );
+
+        }
+
+        public override bool Send( RockMessage rockMessage, int mediumEntityTypeId, Dictionary<string, string> mediumAttributes, out List<string> errorMessages )
+        {
+            errorMessages = new List<string>();
+
+            var sendMessageResult = AsyncHelper.RunSync( () => SendAsync( rockMessage, mediumEntityTypeId, mediumAttributes ) );
+
+            errorMessages.AddRange( sendMessageResult.Errors );
+
+            return !errorMessages.Any();
+        }
+
         public override void Send( Communication communication, int mediumEntityTypeId, Dictionary<string, string> mediumAttributes )
         {
             using ( var rockContext = new RockContext() )
@@ -77,6 +117,49 @@ namespace org.crossingchurch.OurRock.Communications.Transport
             }
 
             base.Send( communication, mediumEntityTypeId, mediumAttributes );
+        }
+
+        private static class AsyncHelper
+        {
+            private static readonly TaskFactory _myTaskFactory = new TaskFactory(
+                CancellationToken.None,
+                TaskCreationOptions.None,
+                TaskContinuationOptions.None,
+                TaskScheduler.Default );
+
+            /// <summary>
+            /// Runs the synchronize.
+            /// </summary>
+            /// <typeparam name="TResult">The type of the result.</typeparam>
+            /// <param name="func">The function.</param>
+            /// <returns></returns>
+            public static TResult RunSync<TResult>( Func<Task<TResult>> func )
+            {
+                var cultureUi = CultureInfo.CurrentUICulture;
+                var culture = CultureInfo.CurrentCulture;
+                return _myTaskFactory.StartNew( () =>
+                {
+                    Thread.CurrentThread.CurrentCulture = culture;
+                    Thread.CurrentThread.CurrentUICulture = cultureUi;
+                    return func();
+                } ).Unwrap().GetAwaiter().GetResult();
+            }
+
+            /// <summary>
+            /// Runs the synchronize.
+            /// </summary>
+            /// <param name="func">The function.</param>
+            public static void RunSync( Func<Task> func )
+            {
+                var cultureUi = CultureInfo.CurrentUICulture;
+                var culture = CultureInfo.CurrentCulture;
+                _myTaskFactory.StartNew( () =>
+                {
+                    Thread.CurrentThread.CurrentCulture = culture;
+                    Thread.CurrentThread.CurrentUICulture = cultureUi;
+                    return func();
+                } ).Unwrap().GetAwaiter().GetResult();
+            }
         }
     }
 }
